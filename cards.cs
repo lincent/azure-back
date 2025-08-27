cdd0b4ba6fc518fbaf7e43eba16e5c0b7987974e
lincent <11511477+lincent@users.noreply.github.com>
2025-08-27 16:56:39 +0100

    initial storage function

---
+++ cards.cs
@@ -0,0 +1,22 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace storage;
+
+public record Card(int Id, string Title, string Content);
+
+public class Cards(ILogger<Cards> logger)
+{
+    private readonly ILogger<Cards> _logger = logger;
+
+    [Function("cards")]
+    public IActionResult Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req,
+    [BlobInput("test/cards.json")] Card[] cards)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation("Returning {count} cards", cards.Length);
+        return new OkObjectResult(cards);
+    }
+}
